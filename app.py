b34d3766de80817f939d607d881f552e89c76543
TommyOBrienGitHub <18097@wc.school,nz>
2022-03-07 09:36:06 +1300

    Create templates

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def render_homepage():
+    #return '<h1>Hello, World!</h1>'
+    return render_template("home.html")
 
+@app.route('/menu')
+def render_menu_page():
+    return render_template("menu.html")
 
-app.run(host='0.0.0.0')
+@app.route('/contact')
+def render_contact():
+    return render_template("contact.html")
+
+# @app.route('/menu')
+# def render_homepage():
+#     return render_template("menu.html")
+#
+# @app.route('/contact')
+# def render_homepage():
+#     return render_template("contact.html")
+
+app.run(host="0.0.0.0")
